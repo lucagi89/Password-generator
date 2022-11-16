09cd6443aa979e9c4b7892442dbb6ebad98aa1b9
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2022-11-16 22:50:06 +0000

    possibility to chose password length

--- index.js
+++ index.js
@@ -6,12 +6,53 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
 "/"];
 let passOne = document.getElementById("pass1-el")
 let passTwo = document.getElementById("pass2-el")
+let five = false
+let ten = false
+let fifteen = false
+
+function fiveGen(){
+  return five = true, ten = false, fifteen = false
+}
+
+function tenGen(){
+    return ten = true, five = false, fifteen = false
+}
+
+function fifteenGen(){
+   return fifteen = true, five = false, ten = false
+}
+
+
+
 
 function generate(){
     
     passOne.textContent = ""
     passTwo.textContent = ""
-    for (let i = 0; i<15; i++){
+    if (five === true){
+      for (let i = 0; i<5; i++){
+         let mathOne =  Math.floor(Math.random()*characters.length)
+      passOne.textContent += characters[mathOne] 
+      }
+  
+      for (let i = 0; i<5; i++){
+          let mathTwo =  Math.floor(Math.random()*characters.length)
+       passTwo.textContent += characters[mathTwo] 
+       //return five = false
+       }
+    }else if (ten === true){
+      for (let i = 0; i<10; i++){
+         let mathOne =  Math.floor(Math.random()*characters.length)
+      passOne.textContent += characters[mathOne] 
+      }
+  
+      for (let i = 0; i<10; i++){
+          let mathTwo =  Math.floor(Math.random()*characters.length)
+       passTwo.textContent += characters[mathTwo] 
+       //return ten = false
+       }
+    }else if(fifteen === true){
+      for (let i = 0; i<15; i++){
        let mathOne =  Math.floor(Math.random()*characters.length)
     passOne.textContent += characters[mathOne] 
     }
@@ -19,7 +60,13 @@ function generate(){
     for (let i = 0; i<15; i++){
         let mathTwo =  Math.floor(Math.random()*characters.length)
      passTwo.textContent += characters[mathTwo] 
+     //return fifteen = false
      }
+    }
+    
+    
+    
+    
 }
 
 
